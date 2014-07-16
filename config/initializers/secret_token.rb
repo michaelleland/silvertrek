4a47cf00d02296d8c984b9c599a474a28326fcff
Michael Kelley <michael@silvertreksystems.com>
2014-07-16 17:19:34 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Silvertrek::Application.config.secret_key_base = 'cc753000f02864f6dade2c66002b47a024dbc54f988aab4062678466af7b71a60481d1b59e488f7425aacfee8ef57c077f1f33f2bf6cacb44a44150298867500'
