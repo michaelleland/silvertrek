3848fa95718aebdf32279320e2ce743552a5f29c
Michael Kelley <michael@silvertreksystems.com>
2015-04-13 16:26:33 +0200

    adds services page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Silvertrek::Application.routes.draw do
   get "static_pages/home"
+  get "services" => "static_pages#services"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
